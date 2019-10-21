7494859317434d7d4e85cda3bf4c5031aa24367a
joesanchezsu <jsanchezsua@ensc.fr>
2019-10-21 19:15:59 +0200

    app finished (as the course) - to improve

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -74,4 +74,14 @@ class PeliculasProvider {
 
     return cast.actores;
   }
+
+  Future<List<Pelicula>> buscarPelicula(String query) async {
+    final url = Uri.https(_url, '3/search/movie', {
+      'api_key': _apikey,
+      'language': _language,
+      'query': query,
+    });
+
+    return await _procesResp(url);
+  }
 }
