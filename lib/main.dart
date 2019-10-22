c380e18edcd60d3ba2c0a9abbadc683b862ca5d7
joesanchezsu <jsanchezsua@ensc.fr>
2019-10-22 19:56:33 +0200

    add API website to reference

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// https://www.themoviedb.org/settings/api
+// https://developers.themoviedb.org/3/getting-started/introduction
+
 import 'package:flutter/material.dart';
 import 'package:movies_app/src/pages/home_page.dart';
 import 'package:movies_app/src/pages/pelicula_detalle_page.dart';
